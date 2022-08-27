e3994bb2bae728b0c9b52f6cbe47c0d7542fc626
Derrick James <ballyouth@gmail.com>
2022-08-27 12:54:17 -0400

    reset to beginning sample code comparison

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,10 @@
 // variables to keep track of quiz state
-var currentQuestionIndex = 0;
+var currentQuestion= 0;
 var time = questions.length * 15;
 var timerId;
 
 // variables to reference DOM elements
-var questionsEl = document.getElementById("questions");
+var questions = document.getElementById("questions");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("choices");
 var submitBtn = document.getElementById("submit");
@@ -18,11 +18,11 @@ var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
 function startQuiz() {
   // hide start screen
-  var startScreenEl = document.getElementById("start-screen");
-  startScreenEl.setAttribute("class", "hide");
+  var startScreen = document.getElementById("start-screen");
+  startScreen.setAttribute("class", "hide");
 
   // un-hide questions section
-  questionsEl.removeAttribute("class");
+  questions.removeAttribute("class");
 
   // start timer
   timerId = setInterval(clockTick, 1000);
@@ -35,11 +35,11 @@ function startQuiz() {
 
 function getQuestion() {
   // get current question object from array
-  var currentQuestion = questions[currentQuestionIndex];
+  var currentQuestion = questions[currentQuestion];
 
   // update title with current question
-  var titleEl = document.getElementById("question-title");
-  titleEl.textContent = currentQuestion.title;
+  var title = document.getElementById("question-title");
+  title.textContent = currentQuestion.title;
 
   // clear out any old question choices
   choicesEl.innerHTML = "";
